c22efb8c67fa76a8341499b90d7058193370dffe
rohithtp <rohithtp@gmail.com>
2025-05-12 20:07:06 +0530

    Add sample route and documentation api

---
+++ app.js
@@ -0,0 +1,44 @@
+// app.js
+   const express = require('express');
+   const swaggerUi = require('swagger-ui-express');
+   const swaggerJSDoc = require('swagger-jsdoc');
+   const userRoutes = require('./routes/user'); // Import user routes
+
+   const app = express();
+   const port = 3000;
+
+   // Swagger definition
+   const swaggerOptions = {
+       swaggerDefinition: {
+           openapi: '3.0.0',
+           info: {
+               title: 'My API',
+               version: '1.0.0',
+               description: 'API documentation using Swagger',
+           },
+           servers: [
+               {
+                   url: `http://localhost:${port}`,
+               },
+           ],
+      components: {
+        securitySchemes: {
+            bearerAuth: {
+                type: 'http',
+                scheme: 'bearer',
+                bearerFormat: 'JWT', 
+            },
+        },
+    },
+       },
+       apis: ['./routes/*.js'], // Path to your API docs
+   };
+
+   const swaggerDocs = swaggerJSDoc(swaggerOptions);
+   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+//    app.use('/users', userRoutes); // Register user routes
+   
+
+   app.listen(port, () => {
+       console.log(`Server running at http://localhost:${port}`);
+   });
---
+++ routes/user.js
@@ -0,0 +1,20 @@
+// routes/user.js
+
+   const express = require('express');
+   const router = express.Router();
+
+   /**
+    * @swagger
+    * /user:
+    *   get:
+    *     summary: Retrieve a list of users
+    *     responses:
+    *       200:
+    *         description: A list of users
+    */
+
+   router.get('/user', (req, res) => {
+       res.status(200).json([{ name: 'John Doe' }, { name: 'Jane Doe' }]);
+   });
+
+   module.exports = router;
